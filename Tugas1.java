5a40d7bb0e46f0a4547c42aa52e40159e54ec0db
ifa6784-arch <shua6784@gmail.com>
2026-04-28 14:13:05 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,21 @@
+
+package tugas1;
+public class Tugas1 {
+
+    public static void main(String[] args) {
+        String[]karakter=new String[6];
+        karakter[0]="A";
+        karakter[1]="B";
+        karakter[2]="C";
+        karakter[3]="D";
+        karakter[4]="E";
+        karakter[5]="F";
+        
+        System.out.println("KARAKTER:");
+        for (int i=0;i< karakter.length;i++){
+            System.out.println("Karakter Ke-"+i+" : "+karakter[i]);
+        }
+                
+    }
+    
+}
