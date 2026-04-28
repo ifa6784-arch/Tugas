4d9858d84e0312b4b38d2b49bfbd89443ed585f7
ifa6784-arch <shua6784@gmail.com>
2026-04-28 14:11:47 +0700

    Add files via upload

---
+++ tugas2.java
@@ -0,0 +1,12 @@
+
+public class tugas2 {
+    public static void main(String[]args){
+        int[]urutan={2,4,6,8,10};
+        System.out.println("URUTAN ANGKA:");
+        for(int i=urutan.length-1;i>=0;i--){
+            System.out.println("Urutan Ke-" +(i+1)+":"+urutan[i]);
+        }
+        
+    }
+    
+}
